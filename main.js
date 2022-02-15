39cedfc0c7b0d8604c69f4a999402556415ffc4a
Sambutcher <Samuel.boucher@gmail.com>
2022-02-15 15:25:04 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,83 +1,102 @@
 
 let capt;
 let img;
-let cw,ch;
-let sw,sh;
+let cw, ch;
+let sw, sh;
 
 let state;
 
 let objectDetector;
 
-const label='traffic light';//'person'
-
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  sw=windowWidth;
-  sh=windowHeight;
-}
-
+const label =/*'traffic light';*/'person';
 
 function setup() {
-  sw=windowWidth;
-  sh=windowHeight;
+  sw = windowWidth;
+  sh = windowHeight;
+  state='loading';
+  createCanvas(sw, sh);
   
-  createCanvas(sw,sh);
-  let constraints = { audio: false, video: { facingMode: "environment" }  };
-  capt=createCapture(constraints, (stream)=>{
-    cw=capt.width;
-    ch=capt.height;
+  let constraints = { audio: false, video: { facingMode: "environment" } };
+  capt = createCapture(constraints, (stream) => {
+    cw = capt.width;
+    ch = capt.height;
     capt.hide();
-    objectDetector = ml5.objectDetector('cocossd', ()=>{
-      state='capture';
+    objectDetector = ml5.objectDetector('cocossd', () => {
+      state = 'capture';
       console.log('OK');
     });
   });
-
 }
 
 function draw() {
-  if (state=='capture') {
-    img=capt.get((cw-ch*sw/sh)/2,0,ch*sw/sh,ch);
-    img.resize(sw,sh);
-    image(img,0,0); 
-    noFill();
-    stroke('white');
-    circle(sw/2,sh/2,sw/10);
-  } 
-  
+  switch (state) {
+    case 'capture':
+      img = capt.get((cw - ch * sw / sh) / 2, 0, ch * sw / sh, ch);
+      img.resize(sw, sh);
+      image(img, 0, 0);
+      noFill();
+      stroke('white');
+      strokeWeight(2);
+      line(sw/2,3*sh/10,sw/2,4*sh/10);
+      line(sw/2,6*sh/10,sw/2,7*sh/10);
+      line(3*sw/10,sh/2,4*sw/10,sh/2);
+      line(6*sw/10,sh/2,7*sw/10,sh/2);
+    break;
+    case 'photo':
+      //
+    break;
+    case 'loading':
+      textAlign(CENTER,CENTER);
+      textSize(32);
+      fill('blue');
+      text('Loading...',sw/2,sh/2);
+    break;
+  }
 }
 
-function distanceToCenter(object){
-  let x=object.x;
-  let y=object.y;
-  let dx=object.width;
-  let dy=object.height;
-  return ((x+dx/2-sw)^2+(y+dy/2-sh)^2);
+function distanceToCenter(object) {
+  let x = object.x;
+  let y = object.y;
+  let dx = object.width;
+  let dy = object.height;
+  return ((x + dx / 2 - sw) ^ 2 + (y + dy / 2 - sh) ^ 2);
 }
 
-function touchStarted(){
-  if (state=='capture'){
-    state='photo';
-    objectDetector.detect(img,(err,results)=>{
+
+function touchStarted() {
+  if (state == 'capture') {
+    state = 'photo';
+
+    image(img,0,0);
+    fill('white');
+    noStroke();
+    circle(sw/2,sh/2,sw/100);
+
+    objectDetector.detect(img, (err, results) => {
       let distMin;
       let cible;
-      for (let i=0;i<results.length;i++){
-        let dist=distanceToCenter(results[i]);
-        if (results[i].label==label && (!distMin || distMin>dist)){
-          cible=results[i];
-          distMin=dist;
+      for (let i = 0; i < results.length; i++) {
+        let dist = distanceToCenter(results[i]);
+        if (results[i].label == label && (!distMin || distMin > dist)) {
+          cible = results[i];
+          distMin = dist;
         }
         if (cible) {
-      fill('red');
-      noStroke();
-      circle(cible.x+cible.width/2,cible.y+cible.height/2,10);
-      console.log(cible);
+          noFill();
+          stroke('blue');
+          rect(cible.x, cible.y, cible.width, cible.height);
         }
-    }
+      }
     });
-  } else if (state=='photo'){
-    state='capture';
+
+  } else if (state == 'photo') {
+    state = 'capture';
   }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  sw = windowWidth;
+  sh = windowHeight;
 }
 
