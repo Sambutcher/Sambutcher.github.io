0d9cbf67b2dbe4cf3e9c4e17aefa4060ebd9fe4f
Sambutcher <Samuel.boucher@gmail.com>
2022-02-28 13:10:17 +0100

    Add files via upload

--- test.js
+++ test.js
@@ -6,9 +6,7 @@ let constraints = { audio: false, video: { facingMode: "environment" } };
 navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {//capture du stream video
   vid.srcObject = stream;
 
-  vid.play().then(() => {
-    console.log('video OK');
-  });
+
 
 });
 
