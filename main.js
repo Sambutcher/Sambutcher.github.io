c8dd9b3390ea3f63677b289ef91c7ff74ed90233
Sambutcher <Samuel.boucher@gmail.com>
2022-03-05 19:00:26 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 let vid = document.getElementById('v');
 let canvas = document.getElementById('c');
 let ctx = canvas.getContext("2d");
-let src,cap;
-let crop;
+let src, cap;
+let feu;
 let vw, vh, W;
-let cw, ch;
-
-let state = 'loading';
+let cw = window.innerWidth;
+let ch = window.innerHeight;
 
 let objectDetector;
-const label = 'traffic light';
 
+canvas.width = cw;
+canvas.height = ch;
+ctx.font = '48px';
+ctx.textAlign = "center";
+ctx.textBaseline = "middle";
+ctx.fillText("Loading...", cw / 2, ch / 2);
 
 //initialisation
 let constraints = { audio: false, video: { facingMode: "environment" } };
@@ -18,20 +22,18 @@ navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {//captu
   vid.srcObject = stream;
   vid.play().then(() => {
     vw = vid.videoWidth;
-    vid.width=vw;
+    vid.width = vw;
     vh = vid.videoHeight;
-    vid.height=vh;
+    vid.height = vh;
     W = vh / vw;
-    cw = window.innerWidth;
-    canvas.width = cw;
-    ch = window.innerHeight;
-    canvas.height = ch;
+
     src = new cv.Mat(vh, vw, cv.CV_8UC4);
     cap = new cv.VideoCapture(vid);
 
     cocoSsd.load().then(model => { //initalisation du modèle de détection
       objectDetector = model;
-      state = 'capture';
+      //lancement de la boucle d'affichage
+      setTimeout(processVideo, 0);
     });
   });
 });
@@ -41,73 +43,29 @@ const FPS = 60;
 function processVideo() {
   let begin = Date.now();
 
-  switch (state) {
-    case 'capture':
-      showFrame();
-      
-      objectDetector.detect(canvas, 20, 0.2).then(result => { // Lancement de la détection
-        //recherche du label le plus au centre ->result[j]
-        let j = -1;
-        let min = cw ** 2 + ch ** 2;
-        for (let i = 0; i < result.length; i++) {
-          let dToC = distanceToCenter(result[i]);
-          if (result[i].class == label && (dToC < min)) {
-            j = i;
-            min = dToC;
-          }
-        }
-        
-        //Recherche et affichage du cercle cible
-        if (j >= 0) {
-          let cible = chercheCible(...result[j].bbox);
-
-          ctx.fillStyle = "black";
-          ctx.fillRect(...result[j].bbox);
-          
-   
-          if (cible) {
-            
-            ctx.beginPath();
-            ctx.arc(cible.x, cible.y, cible.r, 0, 2 * Math.PI, false);
-            ctx.fillStyle = 'white';
-            ctx.fill();
-          }
-        }
-
-        showCross();
-      })
-  
-      
-      break;
-    case 'photo':
-      //
-      break;
-    case 'loading': //Affichage de loading
-      ctx.font = '48px serif';
-      ctx.textAlign="center";
-      ctx.textBaseline = "middle";
-      ctx.fillText("Loading...", cw/2, ch/2);
-      break;
-  }
+  showFrame();
+  detectFeu();
+  showFeu()
+  showCross();
 
   let delay = 1000 / FPS - (Date.now() - begin);
   setTimeout(processVideo, delay);
 }
-//lancement de la boucle d'affichage
-setTimeout(processVideo, 0);
+
 
 //Click event (passage de capture <->photo)
 canvas.addEventListener('click', () => {
-  if (state == 'capture') {
-    state = 'photo';
-    ctx.beginPath();
-            ctx.arc(cw/2, ch/2, 5, 0, 2 * Math.PI, false);
-            ctx.fillStyle = 'red';
-            ctx.fill();
-    
-  } else if (state == 'photo') {
-    state = 'capture';
-  }
+  if (feu){
+    if (Math.sqrt((feu.x + feu.dx / 2 -cw/2)**2+ (feu.y + feu.dy / 6 - ch/2)**2)<feu.dy/8){
+      feu.status[0]=true;
+    } 
+    if (Math.sqrt((feu.x + feu.dx / 2 -cw/2)**2+ (feu.y + feu.dy / 2 - ch/2)**2)<feu.dy/8){
+      feu.status[1]=true;
+    } 
+    if (Math.sqrt((feu.x + feu.dx / 2 -cw/2)**2+ (feu.y + 5*feu.dy / 6 - ch/2)**2)<feu.dy/8){
+      feu.status[2]=true;
+    } 
+   }
 })
 
 //resize event pour modifier les variables de width/height
@@ -127,8 +85,91 @@ function distanceToCenter(object) {
   return ((x + dx / 2 - cw / 2) ** 2 + (y + dy / 2 - ch / 2) ** 2);
 }
 
+
+
+function showFrame() {
+  //Affichage de la frame
+  cap.read(src);
+  let rect = new cv.Rect(Math.max(0, (vw - cw * W) / 2), 0, Math.min(vw, vh * cw / ch), vh);
+  let dst = src.roi(rect);
+  cv.resize(dst, dst, new cv.Size(cw, ch));
+  cv.imshow(canvas, dst);
+  dst.delete();
+}
+
+function showCross() {
+  ctx.beginPath();
+  ctx.moveTo(cw / 2, 4 * ch / 10 - cw / 10);
+  ctx.lineTo(cw / 2, 4 * ch / 10);
+  ctx.moveTo(cw / 2, 6 * ch / 10);
+  ctx.lineTo(cw / 2, 6 * ch / 10 + cw / 10);
+  ctx.moveTo(3 * cw / 10, ch / 2);
+  ctx.lineTo(4 * cw / 10, ch / 2);
+  ctx.moveTo(6 * cw / 10, ch / 2);
+  ctx.lineTo(7 * cw / 10, ch / 2);
+  ctx.lineWidth = 2;
+  ctx.strokeStyle = 'grey';
+  ctx.stroke();
+}
+
+function detectFeu() {
+  objectDetector.detect(canvas, 20, 0.2).then(result => { // Lancement de la détection
+    //recherche du label le plus au centre ->result[j]
+    let j = -1;
+    let min = cw ** 2 + ch ** 2;
+    for (let i = 0; i < result.length; i++) {
+      let dToC = distanceToCenter(result[i]);
+      if (result[i].class == 'traffic light' && (dToC < min)) {
+        j = i;
+        min = dToC;
+      }
+    }
+
+
+    //Recherche et affichage du cercle cible
+    if (j >= 0) {
+      if (!feu) {
+        feu = { 'x': result[j].bbox[0], 'y': result[j].bbox[1], 'dx': result[j].bbox[2], 'dy': result[j].bbox[3], 'status': [false, false, false] };
+      } else {
+        feu = { 'x': result[j].bbox[0], 'y': result[j].bbox[1], 'dx': result[j].bbox[2], 'dy': result[j].bbox[3], 'status': feu.status };
+      }
+    } else {
+      feu = null;
+    }
+
+  })
+}
+
+function showFeu(){
+  if (feu) {
+    ctx.fillStyle = "white";
+    ctx.fillRect(feu.x, feu.y, feu.dx, feu.dy);
+
+    ctx.beginPath();
+    ctx.arc(feu.x + feu.dx / 2, feu.y + feu.dy / 6, feu.dy / 8, 0, 2 * Math.PI, false);
+    ctx.fillStyle = (feu.status[0] ? 'white' : 'black');
+    ctx.strokeStyle = 'black';
+    ctx.fill();
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(feu.x + feu.dx / 2, feu.y + feu.dy / 2, feu.dy / 8, 0, 2 * Math.PI, false);
+    ctx.fillStyle = (feu.status[1] ? 'white' : 'black');
+    ctx.strokeStyle = 'black';
+    ctx.fill();
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(feu.x + feu.dx / 2, feu.y + 5 * feu.dy / 6, feu.dy / 8, 0, 2 * Math.PI, false);
+    ctx.fillStyle = (feu.status[2] ? 'white' : 'black');
+    ctx.strokeStyle = 'black';
+    ctx.fill();
+    ctx.stroke();
+  }
+}
+
 //Recherche de la cible dans le feu
-function chercheCible(x, y, dx, dy) {
+/*function chercheCible(x, y, dx, dy) {
 
   let crop;
 
@@ -140,7 +181,7 @@ function chercheCible(x, y, dx, dy) {
   let low1 = new cv.Mat(crop.rows, crop.cols, crop.type(), [160, 100, 50, 0]);
   let high1 = new cv.Mat(crop.rows, crop.cols, crop.type(), [179, 255, 255, 255]);
   cv.inRange(crop, low1, high1, mask1);
-  
+
   let mask2 = new cv.Mat();
   let low2 = new cv.Mat(crop.rows, crop.cols, crop.type(), [0, 100, 50, 0]);
   let high2 = new cv.Mat(crop.rows, crop.cols, crop.type(), [70, 255, 255, 255]);
@@ -174,35 +215,10 @@ function chercheCible(x, y, dx, dy) {
   contours.delete();
   hierarchy.delete();
 
-  
+
   if (maxCircle) {
     return { 'x': maxCircle.center.x + x, 'y': maxCircle.center.y + y, 'r': maxCircle.radius };
   } else {
     return;
   }
-}
-
-function showFrame(){
-  //Affichage de la frame
-  cap.read(src);
-  let rect= new cv.Rect(Math.max(0,(vw - cw * W) / 2), 0,Math.min(vw, vh * cw / ch), vh);
-  let dst=src.roi(rect);
-  cv.resize(dst,dst,new cv.Size(cw,ch));
-  cv.imshow(canvas, dst);
-  dst.delete();
-}
-
-function showCross(){
-  ctx.beginPath();
-      ctx.moveTo(cw / 2, 4 * ch / 10 - cw / 10);
-      ctx.lineTo(cw / 2, 4 * ch / 10);
-      ctx.moveTo(cw / 2, 6 * ch / 10);
-      ctx.lineTo(cw / 2, 6 * ch / 10 + cw / 10);
-      ctx.moveTo(3 * cw / 10, ch / 2);
-      ctx.lineTo(4 * cw / 10, ch / 2);
-      ctx.moveTo(6 * cw / 10, ch / 2);
-      ctx.lineTo(7 * cw / 10, ch / 2);
-      ctx.lineWidth = 1;
-      ctx.strokeStyle = 'white';
-      ctx.stroke();
-}
+}*/
